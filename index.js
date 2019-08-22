a374c1118f749af183fdd67db0fbc9a88bf58725
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-22 04:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ function theBeatlesPlay(players,instruments) {
 }
 
 function johnLennonFacts() {
-  var facts=[
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  var facts=["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
 var newFacts=[];
 var i;
 while(i < facts.length){
