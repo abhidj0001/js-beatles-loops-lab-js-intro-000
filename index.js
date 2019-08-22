3996352c7881a8a33b217040c3365ba894f2c3a7
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-22 04:36:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(player,instrument) {
+  var allPlayers=[];
+  for(var i=0;i<players.length;i++){
+  allPlayers.push(players[i] + "plays" + instrument[i])
+  }
+  return allPlayers;
+}
