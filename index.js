3b53249de248f3cf3d0926dcced4dc4db7355b14
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-22 04:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function theBeatlesPlay(players,instruments) {
   }
   return allPlayers;
 }
+
+function johnLennonFacts() {
+  var facts=[
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+var newFacts=[];
+var i;
+while(i < facts.length){
+  newFacts.push(facts[i] +"!!!");
+  i++;
+}
+return newFacts;
+}
